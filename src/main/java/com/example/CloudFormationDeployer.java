064b3eb6a027e43ac8608de7b359c51b05d9400f
Bhanu Prakash <prakashg@techmologics.com>
2025-12-29 18:43:12 +0530

    5th commit

--- src/main/java/com/example/CloudFormationDeployer.java
+++ src/main/java/com/example/CloudFormationDeployer.java
@@ -7,7 +7,8 @@ import java.nio.file.Paths;
 
 public class CloudFormationDeployer {
     
-    private static final String TEMPLATE_PATH = "src/main/resources/iam-template.yml";
+    // Updated path to the moved template
+    private static final String TEMPLATE_PATH = "src/main/resources/cloudformation/iam-template.yml";
     private static String stackName;
 
     public static void main(String[] args) {
